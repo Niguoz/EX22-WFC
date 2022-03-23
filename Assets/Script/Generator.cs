b9b5bb8e683c34abb1117ed77f846584ef7917e5
Niguoz <nicola.pellegrini96@tiscali.it>
2022-03-23 12:51:29 +0100

    things

--- Assets/Script/Generator.cs
+++ Assets/Script/Generator.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using DeBroglie;
+using DeBroglie.Models;
+using DeBroglie.Topo;
 using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Define some sample data
+        ITopoArray<char> sample = TopoArray.Create(new[]
+        {
+            new[]{ '_', '_', '_'},
+            new[]{ '_', '*', '_'},
+            new[]{ '_', '_', '_'},
+        }, periodic: false);
+        // Specify the model used for generation
+        var model = new AdjacentModel(sample.ToTiles());
+        // Set the output dimensions
+        var topology = new GridTopology(10, 10, periodic: false);
+        // Acturally run the algorithm
+        var propagator = new TilePropagator(model, topology);
+        var status = propagator.Run();
+        if (status != DeBroglie.Resolution.Decided) throw new Exception("Undecided");
+        var output = propagator.ToValueArray<char>();
+        // Display the results
+        for (var y = 0; y < 10; y++)
+        {
+            for (var x = 0; x < 10; x++)
+            {
+                Debug.Log(output.Get(x, y));
+            }
+            Debug.Log("############");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
